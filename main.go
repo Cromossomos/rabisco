2c4b6a08da8b0e1a783e79bdaf1b7bc9b4ce03bb
Cromo <carol.simoes@aluno.senai.br>
2025-04-03 08:20:41 -0300

    Vector

--- main.go
+++ main.go
@@ -3,25 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-
-	fmt.Print("Digite um dos números: ")
-	fmt.Scan(&a)
-
-	fmt.Print("Digite o outro número: ")
-	fmt.Scan(&b)
-
-
-	fmt.Println("A soma é: ", a + b)
-	fmt.Println("A subtração é:", a - b)
-	fmt.Println("A divisão é:", a / b)
-	fmt.Println("A multiplicação é: ", a * b)
-	fmt.Println("O resto da divisão é: ", a % b)
-
-	a++
-	fmt.Println("Incrementar a", a)
-
-	if a > 0 && b > 0{
-		fmt.Println("Números Positivos")
-	}
+	var ages = [4]int{17, 20, 40, 87}
+	nomes := [4]string{"Mario", "Deadpool", "Hermes", "Coiso"}
+	fmt.Println(ages)
+	fmt.Println(nomes)
+	nomes[3] = "Algo"
+	fmt.Println(nomes)
 }
